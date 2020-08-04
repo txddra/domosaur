1a164207ce6faa09b12bdc75903e1b0f6c6d1b21
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-08-04 14:13:04 -0400

    hidden

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
 const sizeSpan = document.querySelector('.mess-with-me');
 sizeSpan.style.fontSize = '3em';
 sizeSpan.style.color = 'green';
+
+const hiddenSaur = document.querySelector('#hide-me')
+hiddenSaur.style.display ='none';
+
