1735629493cece3f7d99696b8f3f4eb240264fa4
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-08-04 14:57:02 -0400

    button

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//queryselectors
 
 const sizeSpan = document.querySelector('.mess-with-me');
 sizeSpan.style.fontSize = '3em';
@@ -10,18 +11,19 @@ hiddenSaur.style.display ='none';
 const firstDino = document.querySelector('#triceratops');
 firstDino.style.width ='324px'
 
+//event listeners
 
 const makeItOrange = function(){
 
     sizeSpan.style.color = 'orange';
-    };
+ };
     
     sizeSpan.addEventListener('click', makeItOrange);
     
 
 const redBorder = function (){
     firstDino.style.border ='thick solid red';
-        }
+}
         
     firstDino.addEventListener('click', redBorder)
 
@@ -31,4 +33,15 @@ const feathered = document.querySelector('#feathers');
 const halfTransparent = function(){
     feathered.style.opacity = '50%';
 }
-feathered.addEventListener('click', halfTransparent);
+    feathered.addEventListener('click', halfTransparent);
+
+
+    const backgroundButton = document.querySelector('#toggle')
+const rowOfDino = document.querySelector('#row')
+
+const changeBackground = function(){
+    rowOfDino.style.backgroundColor = 'palegoldenrod'
+}
+
+backgroundButton.addEventListener('click', changeBackground)
+
