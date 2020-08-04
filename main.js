a9a726fa7ddbe329ff0a6d582bdd6795da55ac3b
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-08-04 14:16:50 -0400

    changed width

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ sizeSpan.style.color = 'green';
 const hiddenSaur = document.querySelector('#hide-me')
 hiddenSaur.style.display ='none';
 
+
+const firstDino = document.querySelector('#triceratops');
+firstDino.style.width ='324px'
